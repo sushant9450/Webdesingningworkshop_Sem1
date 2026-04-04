1192de4fb9ad977fd8fbda9a0bfea9def351b6ee
sushant9450 <sushantgkp9450@gmail.com>
2026-04-04 21:53:03 +0530

    Add files via upload

---
+++ expfour.js
@@ -0,0 +1,76 @@
+// function calculateResult(){
+//     let n=document.getElementById("Subject").value;
+//     let total=0;
+//     for(let i=1;i<=n;i++){
+//         let marks=parseFloat(prompt("Enter marks for Subject"+i+":"));
+//         total=total+marks;
+//     }
+//     let average=total/n;
+//     let grade;
+//     let result;
+//     if(average>=90){
+//         grade="A+";
+//     }else if(average>=75){
+//         grade="A";
+//     }else if(average>=55){
+//         grade="B";
+//     }else if(average>=45){
+//         grade="C";
+//     }else{
+//         grade="D";
+//     }
+
+//     if(average>=45){
+//         result="Pass";
+//     }else{
+//         result="Fail"
+//     }
+//     document.getElementById("result").innerHTML=
+//      "Total Marks:"+ total +"<br>"+
+//      "Average Marks:"+average.toFixed(2)+"<br>"+
+//      "Grade:"+grade+"<br>"+
+//      "result:"+result; 
+// }
+
+
+
+
+
+
+function calculateResult(){
+let n = document.getElementById("subjects").value;
+let total = 0;
+for(let i=1;i<=n;i++){
+let marks = parseFloat(prompt("Enter marks for Subject " + i));
+total = total + marks;
+}
+let average = total / n;
+let grade;
+let result;
+if(average >= 90){
+grade = "A+";
+}
+else if(average >= 75){
+grade = "A";
+}
+else if(average >= 60){
+grade = "B";
+}
+else if(average >= 50){
+grade = "C";
+}
+else{
+grade = "F";
+}
+if(average >= 40){
+result = "PASS";
+}
+else{
+result = "FAIL";
+}
+document.getElementById("result").innerHTML =
+"Total Marks: " + total + "<br>" +
+"Average Marks: " + average.toFixed(2) + "<br>" +
+"Grade: " + grade + "<br>" +
+"Result: " + result;
+}
